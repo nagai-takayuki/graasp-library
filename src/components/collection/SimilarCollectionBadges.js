0bda9129167881c075323fe292ed33d7dead5455
Hagop Taminian <hagopt@Hagops-MacBook-Air.local>
2020-05-02 12:24:02 +0300

    improvement: added 'similar collections' to graasp explore
    I added dummy 'similar collection' data to src/data/sample, which I used in a new 'similar collections' section at the bottom of the Graasp Explore collections page. The 'similar collections' follows the component pattern of other sections of the application (SimilarCollections component which renders a SimilarCollectionsHeader and individual SimilarCollection components).
    
    closes #7

---
+++ src/components/collection/SimilarCollectionBadges.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Badge from '@material-ui/core/Badge';
+import {
+  Favorite,
+  Visibility,
+  Email,
+  Facebook,
+  Twitter,
+} from '@material-ui/icons';
+import { Rating } from '@material-ui/lab';
+import PropTypes from 'prop-types';
+import { Grid, IconButton } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  badges: {
+    '& > *': {
+      marginRight: theme.spacing(3),
+    },
+  },
+  root: {
+    marginBottom: theme.spacing(),
+  },
+  cell: {
+    display: 'flex',
+  },
+}));
+
+function SimilarCollectionBadges({ views, likes, rating }) {
+  const classes = useStyles();
+
+  const { count, value } = rating;
+
+  return (
+    <Grid container className={classes.root}>
+      <Grid
+        item
+        xs={12}
+        className={classes.cell}
+        justify="flex-start"
+        alignItems="center"
+      >
+        <div className={classes.badges}>
+          <Badge badgeContent={count} color="secondary" max={999}>
+            <Rating value={value} />
+          </Badge>
+          <Badge badgeContent={likes} color="secondary" max={999}>
+            <Favorite color="primary" />
+          </Badge>
+          <Badge badgeContent={views} color="secondary" max={999}>
+            <Visibility color="primary" />
+          </Badge>
+        </div>
+      </Grid>
+    </Grid>
+  );
+}
+
+SimilarCollectionBadges.propTypes = {
+  views: PropTypes.number,
+  likes: PropTypes.number,
+  rating: PropTypes.shape({
+    count: PropTypes.number,
+    value: PropTypes.number,
+  }),
+};
+
+SimilarCollectionBadges.defaultProps = {
+  views: 0,
+  likes: 0,
+  rating: {
+    count: 0,
+    value: 0,
+  },
+};
+
+export default SimilarCollectionBadges;
---
+++ src/components/collection/SimilarCollections.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useTranslation } from 'react-i18next';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import SimilarCollection from './SimilarCollection';
+import SimilarCollectionsHeader from './SimilarCollectionsHeader';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+function SimilarCollections({ similarCollections }) {
+  const { t } = useTranslation();
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <SimilarCollectionsHeader />
+      {!similarCollections.length ? (
+        <div className="Main">
+          <Typography variant="h5" color="inherit">
+            {t('This collection is empty.')}
+          </Typography>
+        </div>
+      ) : (
+        <Grid container spacing={2}>
+          {similarCollections.map((similarCollection) => (
+            <Grid
+              key={similarCollection.id}
+              item
+              xs={12}
+              sm={4}
+              md={3}
+              lg={3}
+              xl={2}
+            >
+              <SimilarCollection similarCollection={similarCollection} />
+            </Grid>
+          ))}
+        </Grid>
+      )}
+    </div>
+  );
+}
+
+// Items.propTypes = {
+//   items: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       image: PropTypes.string,
+//       description: PropTypes.string,
+//       viewLink: PropTypes.func,
+//       id: PropTypes.string,
+//       name: PropTypes.string,
+//     }),
+//   ),
+// };
+
+// Items.defaultProps = {
+//   items: [],
+// };
+
+export default SimilarCollections;
