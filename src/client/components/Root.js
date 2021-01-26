c7dbba7d69acc4791390a7fd3b6f1f45841c2696
pyphilia <pyphilia@gmail.com>
2021-01-26 16:47:55 +0100

    style: remove body padding, add margin to home elements

--- src/client/components/Root.js
+++ src/client/components/Root.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import ReactGa from 'react-ga';
 import { I18nextProvider } from 'react-i18next';
@@ -20,6 +20,10 @@ import { REACT_APP_GOOGLE_ANALYTICS_ID } from '../config/env';
 ReactGa.initialize(REACT_APP_GOOGLE_ANALYTICS_ID);
 
 const styles = {
+  body: {
+    margin: 0,
+    padding: 0,
+  },
   root: {
     flexGrow: 1,
     height: '100%',
@@ -52,20 +56,27 @@ let theme = createMuiTheme({
 
 theme = responsiveFontSizes(theme);
 
-const Root = ({ classes }) => (
-  <div className={classes.root}>
-    <MuiThemeProvider theme={theme}>
-      <I18nextProvider i18n={i18nConfig}>
-        <App />
-        <ToastContainer />
-      </I18nextProvider>
-    </MuiThemeProvider>
-  </div>
-);
+const Root = ({ classes }) => {
+  useEffect(() => {
+    document.body.className = classes.body;
+  });
+
+  return (
+    <div className={classes.root}>
+      <MuiThemeProvider theme={theme}>
+        <I18nextProvider i18n={i18nConfig}>
+          <App />
+          <ToastContainer />
+        </I18nextProvider>
+      </MuiThemeProvider>
+    </div>
+  );
+};
 
 Root.propTypes = {
   classes: PropTypes.shape({
     root: PropTypes.string,
+    body: PropTypes.string.isRequired,
   }).isRequired,
 };
 
--- src/client/components/home/Home.js
+++ src/client/components/home/Home.js
@@ -1,14 +1,22 @@
+import { makeStyles } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import React, { useContext } from 'react';
 import { useTranslation } from 'react-i18next';
 import CollectionsGrid from '../collection/CollectionsGrid';
 import { CollectionContext } from '../CollectionProvider';
 
+const useStyles = makeStyles((theme) => ({
+  wrapper: {
+    padding: theme.spacing(5),
+  },
+}));
+
 function Home() {
   const { t } = useTranslation();
   const { collections } = useContext(CollectionContext);
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.wrapper}>
       <Typography variant="h2">{t('Home')}</Typography>
       <CollectionsGrid collections={collections} />
     </div>
